4c7209154d123e068c96ee908eaabd55c35a2fd4
rukije <rukije.morina@harrisia.com>
2024-07-05 16:09:56 +0200

    Outputting a list of expenses

---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,16 @@
+import { FlatList } from "react-native";
+import { Text } from "react-native";
+
+function renderExpenseItem(itemData) {
+  return <Text>{itemData.item.description}</Text>;
+}
+function ExpensesList({expenses}) {
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={renderExpenseItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
+}
+export default ExpensesList;
