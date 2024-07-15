57376e05603b33fded25601df12be6cd639c2fe2
rukije <rukije.morina@harrisia.com>
2024-07-15 17:02:08 +0200

    Created Overfall Form, created Manage Expense input forms, configuring the form input elements, and stylied the inputs

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-
 import ManageExpense from './screens/ManageExpense';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
